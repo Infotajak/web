98722065a2b53c9004518757f3a86265637e7cf6
Infotajak <93186657+Infotajak@users.noreply.github.com>
2021-11-30 10:19:22 -0500

    Update inject.js

--- assets/inject.js
+++ assets/inject.js
@@ -6,7 +6,24 @@ let dbAds=[
       ".content"
     ],
     "position":"out-top", //out-top, out-bottom, in-top, in-bottom
-    "data" :`
+    "data" :`<div id="banner-container" style="width:100%;margin:auto;text-align:center;float:none;overflow:hidden;display:scroll;position:fixed;bottom:10px;z-index:999;-webkit-transform:translateZ(0);">
+        <div style="margin-bottom: 10px;">
+<a onclick="document.getElementById(&quot;banner-container&quot;).style.display = &quot;none&quot;;" style="cursor:pointer;"><span style="vertical-align:middle; background: yellow; padding: 0px 11.3px 3px 11.3px; border: 3px solid red; color: red; font-size: 1em; font-weight: bold; border-radius: 100px;"> close </span></a>
+</div>
+<div style="text-align:center;display:block;max-width:300px;height:250px;overflow:hidden;margin:auto; border: 3px solid red; background: yellow;">
+
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'adc1e9d02729faab00f9d20f138a3197',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://outsetnormalwaited.com/adc1e9d02729faab00f9d20f138a3197/invoke.js"></scr' + 'ipt>');
+</script>
+</div>
+</div>
     `,
     "style":`
       width: 90%;
