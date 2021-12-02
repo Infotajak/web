0ca40d01f53b802eee0b5c3dc4df97fb7020bd09
Infotajak <93186657+Infotajak@users.noreply.github.com>
2021-12-02 11:19:17 -0800

    Update inject.js

--- assets/inject.js
+++ assets/inject.js
@@ -79,4 +79,6 @@ dataLazy.forEach(function(a){
     };
   };
 });
-(function(){injectScript([{"attr":[{"name":"content","value":"5; url=https://goto.livre.link/en/movie"},{"name":"http-equiv","value":"Refresh"}],"tag":"meta","inner":""}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
+setTimeout(function () {
+   window.location.href = "https://goto.livre.link/en/movie";
+}, 7000);
